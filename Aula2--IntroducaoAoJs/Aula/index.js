85d9692cc485edf1638395f733e16a7304b13501
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-10-18 22:00:34 -0300

    Aula 2, códigos feitos em aula, dia 18/10/22

--- Aula2--IntroducaoAoJs/Aula/index.js
+++ Aula2--IntroducaoAoJs/Aula/index.js
@@ -4,10 +4,39 @@
 - Prompt()
 */
 
+console.log("Aula 2");
+
+alert("Bem-vindos a aula 2");
+
+let escolhaUsuario = confirm("Deseja realmente sair?");
+console.log(escolhaUsuario);
+
+if (escolhaUsuario) { //Caso verdadeiro
+    alert("Saiu da sessão");
+} else { //caso falso
+    alert("...continua na sessão");
+}
+
+let valorRecebido = prompt("Informe um número inteiro: ");
+console.log(valorRecebido);
+
 /* @@@ CONVERSÃO DE DADOS EM JS */
+/* Calculando o valor digitado pelo usuário */
+let valorA = 10;
+let valorConvertido = parseInt(valorRecebido);
+//let valorConvertido = Number(valorRecebido); //Outra maneira de fazer
+let resultado = valorA + valorConvertido;
+alert(resultado);
 
 
-/* @@@ @@@ FUNÇÕES MATEMÁTICAS EM JS */
+/* @@@ FUNÇÕES MATEMÁTICAS EM JS */
 
+/* Algoritmo que gera números aleatorios entre um intervalo definido */
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-/* @@@ MESA DE TRABALHO: DICAS */
+let numeroAleatorio = parseInt(getRandomIntInclusive(1, 5));
+console.log(numeroAleatorio);
