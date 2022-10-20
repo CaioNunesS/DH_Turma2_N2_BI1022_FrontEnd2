110ecf5e9fd237b825e3aa52ee9cba2401bfde14
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-10-19 22:42:22 -0300

    Aula 3

--- Aula3--Revisao_I/Aula/scripts/aula3_script.js
+++ Aula3--Revisao_I/Aula/scripts/aula3_script.js
@@ -2,12 +2,59 @@
 
 // FOR IN
 
-//Laço For simples
+//Exportar o script
 
-//FOR OF
+function estudandoArrays() {
 
-//ForEach
+let usuario = {
+    nome: "Marcos M",
+    idade: 25,
+    endereco: {
+        rua: "Rua tal",
+        numero: 879,
+        bairro: "centro"
+    }
+}
 
-//Map()
+//alert(usuario.idade);
 
-// @@ Importando e exportando scripts
+for (let prop in usuario) {
+    console.log(usuario[prop]);
+}
+
+//Criando novo array -> Nomes de alunos
+
+let arrayAlunos = ["Werlon", "Mauricio", "Pedro"];
+
+arrayAlunos.push("Vanessa", "Lucas");
+
+for (let i = 0; i < arrayAlunos.length; i++) {
+    console.log(arrayAlunos[i]);
+}
+
+//For of
+for (let aluno of arrayAlunos) {
+    console.log(aluno);
+}
+
+console.log("\n");
+
+/* function exibeNomeUsuario(nomeRecebido) {
+    console.log(nomeRecebido);
+} */
+
+let novoArray = arrayAlunos.map(aluno => {
+    return aluno.toUpperCase();
+    }
+);
+
+console.log(novoArray);
+
+}
+
+function nomeUsuario() {
+    console.log("Olá usuário");
+};
+
+/* Exportando as funções que serão lidas por outro script */
+export {estudandoArrays, nomeUsuario};
---
+++ Aula3--Revisao_I/Aula/scripts/outro_script.js
@@ -0,0 +1,8 @@
+//Importa o script
+
+/* Importando e dando um apelido para as funções */
+import {estudandoArrays as estudando , nomeUsuario as name} from './aula3_script.js';
+
+/* Chamando as funções */
+estudando();
+name();
