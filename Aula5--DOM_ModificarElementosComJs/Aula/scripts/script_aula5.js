698d1a2a3c3874b4af40043a627223d082ac50ac
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-10-25 22:03:22 -0300

    Aula 5, códigos feitos em aula, dia 25/10/22

--- Aula5--DOM_ModificarElementosComJs/Aula/scripts/script_aula5.js
+++ Aula5--DOM_ModificarElementosComJs/Aula/scripts/script_aula5.js
@@ -1,10 +1,26 @@
 //@@@@@@ [1] - Modelos de texto  - Template Strings
 
+let usuario = "Marcos";
+function boasvindas() {
+    return "Boas-vindas Marcos";
+}
+alert(`Bem vindo usuário ${usuario}`);
+alert(`O resultado da soma é:  ${boasvindas()}`);
+
 
 //@@@@@@ [2] - Modificando elementos com o DOM
+let h1 = document.querySelector("h1");
+h1.innerHTML = "<h3>Aula 5</h3>";
+console.log(h1.innerText);
+console.log(h1.innerHTML);
 
 
 //@@@@@@ [3] - Modificando estilos 
+h1.style.color = "#0FFF23";
+h1.style.textAlign = "center"
 
 
 //@@@@@@ [4] Modificando a classe dos elementos
+h1.classList.add("novoh1");
+let valid = h1.classList.contains("novoh1");
+console.log(h1);
