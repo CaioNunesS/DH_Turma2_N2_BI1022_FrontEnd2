b5d489f5dd9f3a6d90473b822d6b34b68c18f71e
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-11-16 22:17:46 -0300

    Aula 14, dia 16/11/22

--- Aula14--JSONeStorage/scripts/aula_14.js
+++ Aula14--JSONeStorage/scripts/aula_14.js
@@ -2,17 +2,49 @@
 
 //@@@ sessionStorage VS localStorage
 
-// Métodos disponíveis
-//@@@ setItem -> Forma de armazenar
+//window.sessionStorage.setItem();
+sessionStorage.setItem("id01", "Aula 14");
 
-//@@@ getItem -> Capturar um registro específico
+localStorage.setItem("id01", "Aula 14");
+localStorage.setItem(1, false); //salva como strings
+localStorage.setItem(2, true);
+localStorage.setItem(3, false);
 
-//@@@ buscar todos os registros?
+localStorage.setItem("id01", "Aula 14 - JSON e Storage");
+sessionStorage.setItem("id01", "Aula 14 - JSON e Storage");
 
-//@@@ removeItem -> remover um registro específico
 
-//@@@ clear -> Remover todos os registros
+//Capturando informações salvas
+let informacao = localStorage.getItem("id01");
+console.log(informacao);
+
+// removendo uma informação do Storage
+localStorage.removeItem("id01");
+
+//remover todos os registros
+
+localStorage.clear();
 
 
 
 /* %%%% Práticando a utilização de JSON no JS */
+
+//objeto literal JS
+let usuario = {
+    nome: "Gustavo",
+    cidade: "São Paulo",
+    uf: "SP"
+}
+
+//Transformando o objeto JS em objeto JSON Textual
+let usuarioJSON = JSON.stringify(usuario);
+
+//Setando no Storage
+localStorage.setItem(1, usuarioJSON);
+
+//Buscando do Storage
+let usuarioRetornoStorage = localStorage.getItem(1);
+
+//Transformando o objeto JSON em objeto JS Literal
+let usuarioRetorno = JSON.parse(usuarioRetornoStorage)
+console.log(usuarioRetorno.uf);
