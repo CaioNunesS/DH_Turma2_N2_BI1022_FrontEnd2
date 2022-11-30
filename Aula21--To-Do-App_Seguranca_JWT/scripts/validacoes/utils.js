70649e32080c1fdd7b73f70e40e1e409efb1ad9b
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-11-30 18:21:39 -0300

    Aula 21, códigos base

---
+++ Aula21--To-Do-App_Seguranca_JWT/scripts/validacoes/utils.js
@@ -0,0 +1,8 @@
+/* Retira espaços iniciais e finais de uma strinf informada */
+function normalizaStringUsandoTrim(textoRecebido) {
+    return textoRecebido.trim();
+}
+
+function baseUrl(){
+    return "https://ctd-fe2-todo-v2.herokuapp.com/v1"
+}
